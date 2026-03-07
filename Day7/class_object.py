199ffbb3828316a0ac068d87d9f0e549a8b9890f
Sneha Kumawat <snehakumawat858@gmail.com>
2026-03-07 10:15:26 +0530

    Add files via upload

---
+++ Day7/class_object.py
@@ -0,0 +1,48 @@
+class Car:
+    # Below variables are known as"properties/states/members".
+    wheelers = 4
+    engine ='petrol'
+    base_speed ='40kmph'
+    max_speed='120kmph'
+    gears =4
+# Below instance are called as objects
+# TATA = Car()
+# # by folllowing the below approach ,we can also add some new propertiis inside an object
+# TATA.air_bags =True
+# TATA.security = 'Level 5'
+
+# # For accessing  the properties,we have to follow the bewlow syntax
+# print(f'No. of wheelers: {TATA.wheelers}')
+# print(f'Engine Type: {TATA.engine}')
+# print(f'Base Speed: {TATA.base_speed}')
+# print(f'No. of manual gears: {TATA.gears}')
+# print(f'Air bags: {TATA.air_bags}')
+
+# print()
+
+
+
+
+# mahindra = Car()
+# print('prperties for mahindra')
+
+# print(f'No. of wheelers: {mahindra.wheelers}')
+# print(f'Engine Type: {mahindra.engine}')
+# print(f'Base Speed: {mahindra.base_speed}')
+# print(f'No. of manual gears: {mahindra.gears}')
+
+# print() #brek dene ke liye
+
+# suzuki = Car()
+# print(f'No. of wheelers: {suzuki.wheelers}')
+# print(f'Engine Type: {suzuki.engine}')
+# print(f'Base Speed: {suzuki.base_speed}')
+# print(f'No. of manual gears: {suzuki.gears}')
+# print()
+
+# toyota =Car()   
+# print(f'No. of wheelers: {toyota.wheelers}')
+# print(f'Engine Type: {toyota.engine}')
+# print(f'Base Speed: {toyota.base_speed}')
+# print(f'No. of manual gears: {toyota.gears}')
+# print()
